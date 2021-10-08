1721e32d409b51268dd2decafefb5728981e6954
unknown <Alamin>
2021-10-08 09:19:59 +0600

    yrlc

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../public/css/bootstrap.min.css';
 import '../public/css/animate.min.css';
 import '../public/css/flaticon.css';
 import '../public/css/fontawesome.min.css';
-import '../node_modules/react-modal-video/css/modal-video.min.css';
 import 'react-accessible-accordion/dist/fancy-example.css';
 import '../public/css/style.css';
 import '../public/css/responsive.css';
